c029ee6b16f5daca1ecb55ac9fcc253d5d3e45b5
Keilor Jimenez Quesada <keilorjimenez95@gmail.com>
2018-03-27 11:29:00 -0600

    Se trabaja ordenando el admin php y dandole forma al dashboard para iniciar la programación de las tareas

--- admin.php
+++ admin.php
@@ -11,7 +11,6 @@ if (@!$_SESSION['username']) {
     <title>Admin</title>
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
     <link href="bootstrap/css/bootstrap.css" rel="stylesheet"/>
-
   </head>
 <body data-offset="40" background="images/fondotot.jpg" style="background-attachment: fixed">
 <div class="container">
@@ -19,7 +18,7 @@ if (@!$_SESSION['username']) {
 <div class="row">
 	<?php
 	include("include/cabecera.php");
-	?>
+    ?>
 </div>
 </header>
 
@@ -31,14 +30,21 @@ if (@!$_SESSION['username']) {
 	<div class="container">
 	  <div class="nav-collapse">
 		<ul class="nav">
-			<li><a href="">Bienvenido <strong><?php echo $_SESSION['nombre'];?></strong> </a></li>
+			<li><a href="">Administrado por <strong><?php echo $_SESSION['nombre'];?></strong> </a></li>
 		</ul>
 		<form action="#" class="navbar-search form-inline" style="margin-top:6px">
 		
 		</form>
 		<ul class="nav pull-right">
-		<li><a href="dashboard.php"> Dashboard </a></li>			 
-		<li><a href="desconectar.php"> Cerrar Cesión </a></li>			 
+			<li>
+				<form action="dashboard.php">
+					<input class="btn btn-danger" type="submit" value="Dashboard" />
+				</form>
+			<li>
+				<form action="desconectar.php">
+					<input class="btn btn-danger" type="submit" value="Cerrar Cesión" />
+				</form>
+			</li>			 
 		</ul>
 	  </div><!-- /.nav-collapse -->
 	</div>
@@ -60,114 +66,78 @@ if (@!$_SESSION['username']) {
 		<div class="row-fluid">
 
 			<?php
-				$username = $_SESSION['username'];
-				require("connect_db.php");
-				$sql=("SELECT correo, contrasena, case when rol = 1 then 'Administrador' else 'Usuario' end FROM usuarios WHERE correo <> '$username' ");
-	
-//la variable  $mysqli viene de connect_db que lo traigo con el require("connect_db.php");
-				$query=mysqli_query($mysqli,$sql);
+			$username = $_SESSION['username'];
+			require("connect_db.php");
+			$sql=("SELECT correo, contrasena, case when rol = 1 then 'Administrador' else 'Usuario' end FROM usuarios WHERE correo <> '$username' ");
+			
+			//la variable  $mysqli viene de connect_db que lo traigo con el require("connect_db.php");
+			$query=mysqli_query($mysqli,$sql);
 
-				echo "<table border='1'; class='table table-hover';>";
-					echo "<tr class='warning'>";
-						echo "<td>Correo</td>";
-						echo "<td>Password</td>";
-						echo "<td>Rol</td>";
-						echo "<td>Cambiar</td>";
-						echo "<td>Eliminar</td>";
-					echo "</tr>";
+			echo "<table border='1'; class='table table-hover';>";
+			echo "<tr class='warning'>";
+			echo "<td>Correo</td>";
+			echo "<td>Password</td>";
+			echo "<td>Rol</td>";
+			echo "<td>Cambiar</td>";
+			echo "<td>Eliminar</td>";
+			echo "</tr>";
 
-			    
-			?>
-			  
-			<?php 
-				 while($arreglo=mysqli_fetch_array($query)){
-				  	echo "<tr class='success'>";
-				    	echo "<td>$arreglo[0]</td>";
-				    	echo "<td>$arreglo[1]</td>";
-				    	echo "<td>$arreglo[2]</td>";
+			while($arreglo=mysqli_fetch_array($query)){
+				echo "<tr class='success'>";
+				echo "<td>$arreglo[0]</td>";
+				echo "<td>$arreglo[1]</td>";
+				echo "<td>$arreglo[2]</td>";
 
-				    	echo "<td><a href='admin.php?id=$arreglo[0]&rol=$arreglo[2]&id_boton=1'><img src='images/actualizar.png' class='img-rounded'></td>";
-						echo "<td><a href='admin.php?id=$arreglo[0]&id_boton=2'><img src='images/eliminar.ol.png' class='img-rounded'/></a></td>";
-						
-
-						
-					echo "</tr>";
-				}
-
+				echo "<td><a href='admin.php?id=$arreglo[0]&rol=$arreglo[2]&id_boton=1'><img src='images/actualizar.png' class='img-rounded'></td>";
+				echo "<td><a href='admin.php?id=$arreglo[0]&id_boton=2'><img src='images/eliminar.ol.png' class='img-rounded'/></a></td>";
+				echo "</tr>";
+			}
 				echo "</table>";
-
-					extract($_GET);
-					if(@$id_boton==1){
-						if(@$rol=='Usuario'){	
-							$sqlUpdateDatos="UPDATE usuarios SET rol = 1 WHERE usuarios.correo = '$id'";
-							$resUpdateDatos=mysqli_query($mysqli,$sqlUpdateDatos);
-							}
-						if(@$rol=='Administrador'){
-							$sqlUpdateDatos="UPDATE usuarios SET rol = 2 WHERE usuarios.correo = '$id'";
-							$resUpdateDatos=mysqli_query($mysqli,$sqlUpdateDatos);
-							}
-							if (!$resUpdateDatos) {
-   							printf("Errormessage1: %s\n", $mysqli->error);
-							} else {
-								echo '<script>alert("Se ha editado los administradores")</script> ';
-								//header('Location: proyectos.php');
-								echo "<script>location.href='admin.php'</script>";
-							}
-	
+			extract($_GET);
+			if(@$id_boton==1){
+				if(@$rol=='Usuario'){	
+					$sqlUpdateDatos="UPDATE usuarios SET rol = 1 WHERE usuarios.correo = '$id'";
+					$resUpdateDatos=mysqli_query($mysqli,$sqlUpdateDatos);
+				}
+				if(@$rol=='Administrador'){
+					$sqlUpdateDatos="UPDATE usuarios SET rol = 2 WHERE usuarios.correo = '$id'";
+					$resUpdateDatos=mysqli_query($mysqli,$sqlUpdateDatos);
+				}
+				if (!$resUpdateDatos) {
+					printf("Errormessage1: %s\n", $mysqli->error);
+				} else {
+					echo '<script>alert("Se ha editado los administradores")</script> ';
+					//header('Location: proyectos.php');
+					echo "<script>location.href='admin.php'</script>";
+				}
+			}
+			if(@$id_boton==2){
+				$sqlborrarDatos="DELETE FROM datos_personales WHERE FK_correo='$id'";
+				$resborrarDatos=mysqli_query($mysqli,$sqlborrarDatos);
+				if (!$resborrarDatos) {
+					printf("Errormessage1: %s\n", $mysqli->error);
+				}else {
+					$sqlborrarUsuario="DELETE FROM usuarios WHERE correo='$id'";
+					$resborrarUsuario=mysqli_query($mysqli,$sqlborrarUsuario);
+					if (!$resborrarUsuario) {
+						printf("Errormessage2: %s\n", $mysqli->error);
+					} else {
+						echo '<script>alert("REGISTRO ELIMINADO")</script> ';
+						//header('Location: proyectos.php');
+						echo "<script>location.href='admin.php'</script>";
 					}
-					if(@$id_boton==2){
-							
-						$sqlborrarDatos="DELETE FROM datos_personales WHERE FK_correo='$id'";
-						$resborrarDatos=mysqli_query($mysqli,$sqlborrarDatos);
-							if (!$resborrarDatos) {
-   							printf("Errormessage1: %s\n", $mysqli->error);
-							}else {
-							$sqlborrarUsuario="DELETE FROM usuarios WHERE correo='$id'";
-							$resborrarUsuario=mysqli_query($mysqli,$sqlborrarUsuario);
-								if (!$resborrarUsuario) {
-		   						printf("Errormessage2: %s\n", $mysqli->error);
-								} else {
-								echo '<script>alert("REGISTRO ELIMINADO")</script> ';
-								//header('Location: proyectos.php');
-								echo "<script>location.href='admin.php'</script>";
-										}
-									}
-					}
-
+				}
+			}
 			?>
 			
-				  
-			  			  
-			  
-		
-		
 		<div class="span8">
-		
 		</div>	
 		</div>	
 		<br/>
-		
-
-
-		<!--EMPIEZA DESLIZABLE-->
-		
-		 <!--TERMINA DESLIZABLE-->
-
-
-
-		
-		
 		</div>
-
-		
-
-
-		
-
 <!--///////////////////////////////////////////////////Termina cuerpo del documento interno////////////////////////////////////////////-->
 </div>
-
-	</div>
+</div>
 </div>
 <!-- Footer
       ================================================== -->
@@ -179,11 +149,5 @@ if (@!$_SESSION['username']) {
  </footer>
 </div><!-- /container -->
 
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="bootstrap/js/jquery-1.8.3.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-	</style>
   </body>
 </html>
--- dashboard.php
+++ dashboard.php
@@ -1,50 +1,87 @@
-<?php
-  session_start();
-  if (@!$_SESSION['username']) {
-    header("Location:index.php");
-  }
-  require("connect_db.php");
-  $user=$_SESSION['username'];
-  $sql=mysqli_query($mysqli, "SELECT nombre FROM datos_personales where FK_correo = '$user'"); 
-      if (!$sql) {
-        printf("Errormessage1: %s\n", $mysqli->error);
-      } else {
-        $datos_usuario=mysqli_fetch_assoc($sql);
-        $_SESSION['nombre']=$datos_usuario['nombre'];
-      }
-?>
 <!DOCTYPE html>
-<html>
+<?php
+session_start();
+if (@!$_SESSION['username']) {
+    header("Location:index.php");
+}
+require("connect_db.php");
+$user=$_SESSION['username'];
+$sql=mysqli_query($mysqli, "SELECT nombre FROM datos_personales where FK_correo = '$user'");
+if (!$sql) {
+printf("Errormessage1: %s\n", $mysqli->error);
+} else {
+$datos_usuario=mysqli_fetch_assoc($sql);
+$_SESSION['nombre']=$datos_usuario['nombre'];
+}
+?>
+<html lang="en">
 <head>
-<meta charset="utf-8">
-    <title>Administrador de Tareas</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
-    <script src="bootstrap/js/jquery-1.8.3.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-	
+	<meta charset="utf-8" />
+	<title>Tareas</title>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
 </head>
-<body background="" style="background-attachment: fixed" >
-  <ul>
-    <li><form action="formulario.php" method="POST">
-          <input class="btn btn-danger" type="submit" name="Accion" value="Editar">
-        </form> 
-    </li>
-    <li>
-  <?php  
-    if($_SESSION['rol']==1){
-        echo '<form action="admin.php">'; 
-        echo '<input  class="btn btn-danger" type="submit" value="Administrar"/>';
-        echo '</form>';
-      }
-  ?>  
-  </li>
-  </ul>
-	<center>
-     <div class="tit">
-        <h2 style="color: #; ">Inicio</h2>
-     </div>
-  </center>
+<body background="images/fondotot.jpg" style="background-attachment: fixed">
+	<div class="container">
+		<header class="header">
+			<div class="row">
+				<?php
+				include("include/cabecera.php");
+                ?>
+			</div>
+		</header>
+
+	<!-- Navbar
+    ================================================== -->
+		<div class="navbar">
+			<div class="navbar-inner">
+				<div class="container">
+					<div class="nav-collapse">
+						<ul class="nav">
+							<li>
+								<a href="">
+									Tareas de 
+									<strong>
+										<?php echo $_SESSION['nombre'];?>
+									</strong>
+								</a>
+							</li>
+						</ul>
+						<form action="#" class="navbar-search form-inline" style="margin-top:6px"></form>
+						<ul class="nav pull-right">
+							<?php
+							if($_SESSION['rol']==1){
+								echo '<li> <form action="admin.php">';
+								echo '<input  class="btn btn-danger" type="submit" value="Administrar"/>';
+								echo '</form> </li>';
+							}
+                            ?>
+							<li>
+							    <form action="formulario.php" method="POST">
+									<input class="btn btn-danger" type="submit" name="Accion" value="Editar" />
+								</form> 
+							<li>
+								<form action="desconectar.php" >
+									<input class="btn btn-danger" type="submit" value="Cerrar Cesión" />
+								</form>
+								<!--<a href="desconectar.php"> Cerrar Cesión </a>-->
+							</li>
+						</ul>
+					</div><!-- /.nav-collapse -->
+				</div>
+			</div><!-- /navbar-inner -->
+		</div>
+
+		<!-- ======================================================================================================================== -->
+		<hr class="soften" />	
+		<footer class="footer">
+			<hr class="soften" />
+			<p>
+				&copy; Copyright Keilor Jiménez
+				<br />
+				<br />
+			</p>
+		</footer>	
+	</div><!-- /container -->
 </body>
 </html>
